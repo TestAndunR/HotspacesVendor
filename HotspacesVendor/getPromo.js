1872063d23689d6aeb9a3fd0855f298d4efed24a
TestAndunR <andunranmal@gmail.com>
2018-10-31 10:44:09 +0530

    add ddb with scan operation

--- HotspacesVendor/getPromo.js
+++ HotspacesVendor/getPromo.js
@@ -1,6 +1,38 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = function(event, context, callback) {
-    console.log(event);
-    callback(null,'Successfully executed');
+exports.handler = function (event, context, callback) {
+    let body = JSON.parse(event.body);
+    let vendor = body.vendorId;
+    ddb.scan({
+        TableName: 'Promotions',
+        ExpressionAttributeValues: {
+            ':vendor': vendor
+        },
+        FilterExpression: 'VendorId = :vendor'
+    }).promise().then(function (data) {
+        callback(null, {
+            "isBase64Encoded": true,
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "*"
+            },
+            "body": JSON.stringify(data.Items)
+        });
+    }).catch(function (err) {
+        //handle error
+        // console.log(err);
+        callback(null, {
+            "isBase64Encoded": true,
+            "statusCode": 502,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "*"
+            },
+            "body": 'Error occured in scanning ' + error.message
+        });
+    });
+    // console.log(event);
+    // callback(null, 'Successfully executed');
 }
