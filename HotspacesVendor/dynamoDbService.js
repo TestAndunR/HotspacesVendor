845c70e1e26a11427045078118554514c1faf1de
TestAndunR <andunranmal@gmail.com>
2018-11-29 14:34:49 +0530

    remove db

--- HotspacesVendor/dynamoDbService.js
+++ HotspacesVendor/dynamoDbService.js
@@ -3,52 +3,55 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 
 module.exports = {
     addPromo: function (promoData) {
-        return ddb.put({
-            TableName: 'Promotions',
-            Item: {
-                'promoId': promoData.promoId,
-                'vendorId': promoData.vendorId,
-                'termsNConditions': promoData.terms,
-                'offerType': promoData.offerType,
-                'startDate': promoData.startDate,
-                'endDate': promoData.endDate,
-                'startTime': promoData.startTime,
-                'endTime': promoData.endTime,
-                'selectedDays': promoData.selectedDays,
-                'description': promoData.description,
-                'unitPrice': promoData.unitPrice,
-                'title': promoData.title,
-                'imgUrls': promoData.imgUrl,
-                'discount': promoData.discount,
-                'category': promoData.businessType,
-                'timestamp': promoData.timestamp,
-                'locationBox': promoData.locationBox,
-                'latNLong': promoData.latNLong
-            }
-        }).promise()
+        
+    }
+    // addPromo: function (promoData) {
+    //     return ddb.put({
+    //         TableName: 'Promotions',
+    //         Item: {
+    //             'promoId': promoData.promoId,
+    //             'vendorId': promoData.vendorId,
+    //             'termsNConditions': promoData.terms,
+    //             'offerType': promoData.offerType,
+    //             'startDate': promoData.startDate,
+    //             'endDate': promoData.endDate,
+    //             'startTime': promoData.startTime,
+    //             'endTime': promoData.endTime,
+    //             'selectedDays': promoData.selectedDays,
+    //             'description': promoData.description,
+    //             'unitPrice': promoData.unitPrice,
+    //             'title': promoData.title,
+    //             'imgUrls': promoData.imgUrl,
+    //             'discount': promoData.discount,
+    //             'category': promoData.businessType,
+    //             'timestamp': promoData.timestamp,
+    //             'locationBox': promoData.locationBox,
+    //             'latNLong': promoData.latNLong
+    //         }
+    //     }).promise()
 
 
-    },
+    // },
 
-    deletePromo: function (promoData) {
-        return ddb.delete({
-            TableName: 'Promotions',
-            Key: {
-                'promoId': promoData.promoID,
-                'timestamp': timestamp
-            }
-        }).promise()
-    },
+    // deletePromo: function (promoData) {
+    //     return ddb.delete({
+    //         TableName: 'Promotions',
+    //         Key: {
+    //             'promoId': promoData.promoID,
+    //             'timestamp': timestamp
+    //         }
+    //     }).promise()
+    // },
 
-    retrievePromos: function (date) {
-        return ddb.scan({
-            TableName: 'Promotions',
-            ExpressionAttributeValues: {
-                ':date': date
-            },
-            FilterExpression: 'startDate <= :date and endDate >= :date'
-        }).promise()
-    },
+    // retrievePromos: function (date) {
+    //     return ddb.scan({
+    //         TableName: 'Promotions',
+    //         ExpressionAttributeValues: {
+    //             ':date': date
+    //         },
+    //         FilterExpression: 'startDate <= :date and endDate >= :date'
+    //     }).promise()
+    // },
 
     // getVendor: function (vendorId) {
     //     return ddb.get({
